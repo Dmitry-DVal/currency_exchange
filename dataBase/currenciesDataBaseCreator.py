0e813017bb123bbd05aa4d9e503cea86af771dbc
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2024-08-22 17:13:34 +0400

    Created currencies table
    The CurrenciesDataBaseCreator class creates currencies table if it doesn't exist

---
+++ dataBase/currenciesDataBaseCreator.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+class CurrenciesDataBaseCreator():
+    """
+    Создает таблицу Currencies, если она ещё не создана
+
+    """
+
+    def __init__(self, currencies_list):
+        self.create_bd(currencies_list)
+
+    def create_bd(self, currencies_list):
+        with sqlite3.connect('currencies') as con:
+            cur = con.cursor()  # курсор
+
+            # Создать таблицу, если она не существует
+            cur.execute("""CREATE TABLE IF NOT EXISTS currencies
+            (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+            Code VARCHAR,
+            FullName VARCHAR, 
+            Sign VARCHAR)""")
+
+            # Проверка, есть ли уже записи в таблице
+            cur.execute("SELECT COUNT(*) FROM currencies")
+            if cur.fetchone()[0] == 00:
+                cur.executemany("INSERT INTO currencies VALUES(NULL, ?, ?, ?)", currencies_list)
+
+
+if __name__ == '__main__':
+    # Начальный список валют
+    currencies_list = [('EUR', 'Euro', '€'),
+                       ('RUB', 'Russian Ruble', '₽'),
+                       ('GEL', 'Georgian Lari', '₾'),
+                       ('CNY', 'Chinese Yuan ', '¥'),
+                       ('USD', 'US Dollar', '$')]
+    CurrenciesDataBaseCreator(currencies_list)
