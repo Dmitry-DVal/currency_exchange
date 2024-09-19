d61da985254a7c071fc61470b74641b9d349947b
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2024-09-19 11:41:33 +0400

    GET requests processing moved to GetHandle

--- controller/getHandler.py
+++ controller/getHandler.py
@@ -1,17 +1,48 @@
 from http import HTTPStatus
-from controller.ourHandler import OurHandler
+from model.currenciesRepository import CureenciesRepository
+from model.currencyRepository import CurrencyRepository
+from view.jsonFormater import JsonFormater
 
 
-class GetHandler(OurHandler):
-    def __init__(self, path):
-        self.path = path
+class GetHandler():
 
-    def do_GET(self):
-        self.get_currencies()
+    @staticmethod
+    def get_currencies(handler):
+        currencies = CureenciesRepository().get_cureencies()
+        json_response = JsonFormater().to_json(currencies)
+        handler.send_response(HTTPStatus.OK)  # Заголовок. Отправляем статут клиенту
+        handler.send_header("Content-Type", 'text/json; charset=UTF-8')  # Передаем заголовок.
+        handler.end_headers()
 
-    def get_currencies(self):
-        self.send_response(HTTPStatus.OK)  # Заголовок. Отправляем статут клиенту
-        self.send_header("Content-Type", 'text/html; charset=UTF-8')  # Передаем заголовок.
-        self.end_headers()
+        handler.wfile.write(json_response.encode("utf-8"))
 
-        self.wfile.write("<h1>Здесь будет весь список валют</h1>".encode("utf-8"))  # Запись ответа клиенту
+
+    @staticmethod
+    def get_currency(handler, currency_code):
+        '''Отправляет результат запроса конкретной валюты.'''
+        currency = CurrencyRepository(currency_code).get_cureency()
+        if not currency:
+            # Если валюта не найдена, отправляем 404 ошибку
+            GetHandler.is_not_currency_code(handler)
+        else:
+            # Если валюта найдена, возвращаем её в формате JSON
+            GetHandler.is_currency_code(handler, currency)
+
+
+    @staticmethod
+    def is_currency_code(handler, currency):
+        json_response = JsonFormater().to_json(currency)
+        handler.send_response(HTTPStatus.OK)
+        handler.send_header("Content-Type", "application/json; charset=UTF-8")
+        handler.end_headers()
+
+        handler.wfile.write(json_response.encode("utf-8"))
+
+    @staticmethod
+    def is_not_currency_code(handler):
+        handler.send_response(HTTPStatus.NOT_FOUND)
+        handler.send_header("Content-Type", "application/json; charset=UTF-8")
+        handler.end_headers()
+
+        error_response = JsonFormater().to_json({"error": "Currency not found"})
+        handler.wfile.write(error_response.encode("utf-8"))
--- controller/ourHandler.py
+++ controller/ourHandler.py
@@ -1,59 +1,20 @@
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
-from view.jsonFormater import JsonFormater
-from model.currenciesRepository import CureenciesRepository
-from model.currencyRepository import CurrencyRepository
+from controller.getHandler import GetHandler
 
 
 
 class OurHandler(BaseHTTPRequestHandler):
-    pass
 
     def do_GET(self):
         if self.path == '/currencies':
-            self.get_currencies()
+            GetHandler.get_currencies(self)
         elif self.path.startswith('/currency/'):
             #Извлечение кода валюты из пути
             currency_code = self.path.split('/')[-1]
-            self.get_currency(currency_code)
+            GetHandler.get_currency(self, currency_code)
         else:
             self.page_not_found()
-
-    def get_currencies(self):
-        currencies = CureenciesRepository().get_cureencies()
-        json_response = JsonFormater().to_json(currencies)
-        self.send_response(HTTPStatus.OK)  # Заголовок. Отправляем статут клиенту
-        self.send_header("Content-Type", 'text/json; charset=UTF-8')  # Передаем заголовок.
-        self.end_headers()
-
-        self.wfile.write(json_response.encode("utf-8"))
-
-    def get_currency(self, currency_code):
-        '''Отправляет результат запроса конкретной валюты.'''
-        currency = CurrencyRepository(currency_code).get_cureency()
-        if not currency:
-            # Если валюта не найдена, отправляем 404 ошибку
-            self.is_not_currency_code()
-        else:
-            # Если валюта найдена, возвращаем её в формате JSON
-            self.is_currency_code(currency)
-
-
-    def is_currency_code(self, currency):
-        json_response = JsonFormater().to_json(currency)
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-Type", "application/json; charset=UTF-8")
-        self.end_headers()
-
-        self.wfile.write(json_response.encode("utf-8"))
-
-    def is_not_currency_code(self):
-        self.send_response(HTTPStatus.NOT_FOUND)
-        self.send_header("Content-Type", "application/json; charset=UTF-8")
-        self.end_headers()
-
-        error_response = JsonFormater().to_json({"error": "Currency not found"})
-        self.wfile.write(error_response.encode("utf-8"))
 
     def page_not_found(self):
         self.send_response(HTTPStatus.NOT_FOUND)
