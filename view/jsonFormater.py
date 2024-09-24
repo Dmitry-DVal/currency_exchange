e43efed2ab99652527f74cc0679ecfc7bacc4389
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2024-09-24 13:12:40 +0400

    ExchangeRates are sent to the client

--- view/jsonFormater.py
+++ view/jsonFormater.py
@@ -1,20 +1,24 @@
 import json
+from model.exchangeRatesRepository import ExchangeRatesRepository
 
 
 class JsonFormater:
-    "Преобразует данные в json"
+    """Преобразует данные в json"""
 
     def to_json(self, data):
         # Проверяем, если данные — это список (например, для валют), обрабатываем их
-        if isinstance(data, list):
-            result = self.make_dict(data)
+        if isinstance(data, list) and len(data[0]) > 6: # Нужна нормальная проверка
+            result = self.make_rates_dict(data)
+            return json.dumps(result, indent=4)
+        elif isinstance(data, list):
+            result = self.make_currency_dict(data)
             return json.dumps(result, indent=4)
         else:
             # Если это словарь или ошибка, возвращаем его напрямую
             return json.dumps(data)
 
     @staticmethod
-    def make_dict(data):
+    def make_currency_dict(data):
         result = []
         for row in data:
             result.append({
@@ -22,5 +26,28 @@ class JsonFormater:
                 "code": row[1],
                 "name": row[2],
                 "sign": row[3]
+            })
+        return result
+
+    @staticmethod
+    def make_rates_dict(data):
+        result = []
+        for row in data:
+            result.append({
+                "id": row[0],
+                "baseCurrency": {
+                    "id": row[1],
+                    "name": row[2],
+                    "code": row[3],
+                    "sign": row[4]
+                },
+                "targetCurrency": {
+                    "id": row[5],
+                    "name": row[6],
+                    "code": row[7],
+                    "sign": row[8]
+                },
+                "rate": row[9]
+
             })
         return result
